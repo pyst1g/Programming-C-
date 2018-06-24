437d2a8fd453bde2d234a5015e5ee69dcec983d5
Tassan <s1511381@s.tsukuba.ac.jp>
2018-06-24 22:44:13 +0900

    arc099

---
+++ AtCoder/arc/arc099/C.cpp
@@ -0,0 +1,52 @@
+// finish date: 2018/06/21
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <bitset>
+#include <algorithm>
+#include <stack>
+#include <queue>
+#include <map>
+#include <climits>
+#include <functional>
+
+using namespace std;
+
+#define FOR(i, a, b) for(int (i)=a;(i)<(b);(i)++)
+#define rep(i, n)  FOR(i,0,n)
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<vector<int>> vvi;
+typedef vector<vector<vector<int>>> vvvi;
+typedef vector<ll> vl;
+typedef vector<vector<ll>> vvl;
+typedef vector<double> vd;
+typedef vector<vector<double>> vvd;
+typedef vector<bool> vb;
+typedef vector<vector<bool>> vvb;
+typedef vector<string> vs;
+typedef vector<char> vc;
+typedef vector<vector<char>> vvc;
+typedef pair<int, int> pii;
+const int bigmod = 1000000007;
+const int INF = 1050000000;
+
+
+int main() {
+    int N, K;
+    cin >> N >> K;
+    vi A(N);
+    int idx = 0;
+    rep(i, N) {
+        cin >> A[i];
+        if (A[i] == 0) idx = i;
+    }
+    cout << (N - 1 +( K-2)) / (K - 1) << endl;
+
+
+    return 0;
+}
+
+
+
+
